9523ffa170ffceb03c67c54cd1a744096c737eea
Eric Bonharme <eric.bonharme@gmail.com>
2017-02-21 00:08:22 +0000

    Switch to mycon's base

--- nestgraph/config.php
+++ nestgraph/config.php
@@ -7,7 +7,9 @@ $config = array(
 	    'db_name' => getenv('MYSQL_DB_NAME'),
 	    'nest_user' => getenv('NEST_USERNAME'),
 	    'nest_pass' => getenv('NEST_PASSWORD'),
-        'local_tz' => getenv('TZ')
+        'local_tz' => getenv('TZ'),
+        'local_woeid' => getenv('WOEID'), //19344 = Edinburgh, Scotland
+        'temperature_units' => getenv('TEMPERATURE_UNITS')
     );
 
 ?>
