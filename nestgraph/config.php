0827765a78cc75ae195c2285929ece860333df5e
Eric Bonharme <eric.bonharme@gmail.com>
2017-03-08 23:55:25 +0000

    Update config.php

--- nestgraph/config.php
+++ nestgraph/config.php
@@ -3,10 +3,10 @@
 $config = array(
         'db_ip' => getenv('MYSQL_HOST'),
         'db_user' => getenv('MYSQL_USER'),
-	    'db_pass' => getenv('MYSQL_PASSWORD'),
-	    'db_name' => getenv('MYSQL_DB_NAME'),
-	    'nest_user' => getenv('NEST_USERNAME'),
-	    'nest_pass' => getenv('NEST_PASSWORD'),
+	'db_pass' => getenv('MYSQL_PASSWORD'),
+	'db_name' => getenv('MYSQL_DB_NAME'),
+	'nest_user' => getenv('NEST_USERNAME'),
+	'nest_pass' => getenv('NEST_PASSWORD'),
         'local_tz' => getenv('TZ'),
         'local_woeid' => getenv('WOEID'), //19344 = Edinburgh, Scotland
         'temperature_units' => getenv('TEMPERATURE_UNITS'),
