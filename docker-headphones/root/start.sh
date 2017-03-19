4188cfcd3a589201db6205a839e97d0bbd4123e3
Eric Bonharme <eric.bonharme@gmail.com>
2017-03-19 23:34:53 +0000

    New headphones image

---
+++ docker-headphones/root/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+[ -d ${DATA_DIR} ] || mkdir -p ${DATA_DIR}
+[ -d ${MUSIC_DIR}] || mkdir -p ${MUSIC_DIR}
+
+echo "Mounting ${NFS_HOST}:${NFS_DATA_PATH} on ${DATA_DIR}"
+mount -o nfsvers=4 ${NFS_HOST}:${NFS_DATA_PATH} ${DATA_DIR}
+
+echo "Mounting ${NFS_HOST}:${NFS_MUSIC_PATH} on ${MUSIC_DIR}"
+mount -o nfsvers=4 ${NFS_HOST}:${MUSIC_PATH} ${MUSIC_DIR}
+
+/init
